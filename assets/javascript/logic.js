ec0a5f61b251b467ca44cbfc5855eb8cc237ae8a
LamarC <dviann89@gmail.com>
2018-09-15 21:42:46 -0500

    Style is done

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+function Quiz(question) {
+    this.score = 0;
+    this.question = question;
+    this.questionIndex = 0;
+}
+
+var name = "My name is is Bob";
+
+ for (i = 0, i > 5, i++) {
+     console.log(name);
+ }
